59d59ea087c6896ce665712078231bcebbc4e1df
Marco Mix <marco.mix@alumni.fh-aachen.de>
2015-05-26 22:12:43 +0200

    Android App unter Versionsverwaltung stellen

---
+++ app/src/main/java/de/fhaachen/mm5623s/emptyapplication/MapFragment.java
@@ -0,0 +1,19 @@
+package de.fhaachen.mm5623s.emptyapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by User on 26.05.2015.
+ */
+public class MapFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.map_fragment, container, false);
+    }
+}
